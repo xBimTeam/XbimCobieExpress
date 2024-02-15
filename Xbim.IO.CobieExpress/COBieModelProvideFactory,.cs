7a2870be5fe6db94ce61c79d125c9b13f8478614
Andy Ward <andy.ward@xbim.net>
2024-02-15 14:20:21 +0000

    Tidied obsolete code

--- Xbim.IO.CobieExpress/COBieModelProvideFactory,.cs
+++ Xbim.IO.CobieExpress/COBieModelProvideFactory,.cs
@@ -8,36 +8,9 @@ using Xbim.Ifc;
 
 namespace Xbim.IO.CobieExpress
 {
-    [Obsolete]
+    [Obsolete("ModelProviders are now created via the XbimServices.Current.ServiceProvider")]
     public class COBieModelProviderFactory : IModelProviderFactory
     {
-        //IModelProviderFactory inner;
-
-        //public COBieModelProviderFactory()
-        //{
-        //    inner = new DefaultModelProviderFactory();
-        //}
-
-        //public IModelProvider CreateProvider()
-        //{
-        //    var modelProvider = inner.CreateProvider();
-
-        //     override the modelProvider to use our COBie EntityFactory
-        //    modelProvider.EntityFactoryResolver = (version) =>
-        //    {
-        //        if (version == Common.Step21.XbimSchemaVersion.Cobie2X4)
-        //        {
-        //            return new EntityFactoryCobieExpress();
-        //        }
-        //        return null;
-        //    };
-        //    return modelProvider;
-        //}
-
-        //public void Use(Func<IModelProvider> providerFn)
-        //{
-        //    inner.Use(providerFn);
-        //}
         public IModelProvider CreateProvider()
         {
             throw new NotImplementedException();
